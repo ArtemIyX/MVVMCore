42479768d38ab9dc429f4292fa64f8df70c6e33c
ArtemIyX <68477399+ArtemIyX@users.noreply.github.com>
2021-07-01 11:23:33 +0300

    Добавьте файлы проекта.

---
+++ MVVMCoreLib/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVMCore
+{
+    public class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        public ViewModel()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        private bool _disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _disposed) return;
+            _disposed = true;
+        }
+        /*~ViewModel()
+        {
+            Dispose(false);
+        }*/
+    }
+}
